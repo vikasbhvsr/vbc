becc454e7c2b960744aaf095504d4a78113b251e
Vikas Bhavsar <vikasbhvsr@gmail.com>
2018-10-05 11:16:06 -0700

    fixed top navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
   header.classList.toggle("show");
 }
+
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () {
+  myFunction()
+};
+
+// Get the header
+var navbar = document.querySelector(".navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
