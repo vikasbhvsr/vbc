ba3d5e416896d4cafbc7e761a4b17c65f6b636e0
Vikas Bhavsar <vikasbhvsr@gmail.com>
2018-10-06 22:32:54 -0700

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,12 @@
-var menuBtn = document.querySelector(".menu-btn");
-var header = document.querySelector(".show");
-menuBtn.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  header.classList.toggle("show");
-}
-
-
+// FIXED NAVBAR //
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function () {
   myFunction()
 };
-
 // Get the header
 var navbar = document.querySelector(".navbar");
-
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
@@ -25,4 +14,14 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+// Navbar toggle in mobile display //
+
+var menuBtn = document.querySelector(".menu-btn");
+var header = document.querySelector("header");
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  header.classList.toggle("show");
 }
