41c5b26f35db68031d7845e396a60647af6bd305
Vikas Bhavsar <vikasbhvsr@gmail.com>
2022-12-26 20:56:05 -0800

    install GTM and code refactor

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,42 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
     return (
         <Html lang='en'>
             <Head>
-                {/* <!-- Primary Meta Tags --> */}
-                <title>Vaidehi Beauty Care & Spa</title>
-                <meta name="title" content="Vaidehi Beauty Care & Spa" />
-                <meta name="description" content="Vaidehi Beauty Care & Spa has been offering beauty services to the lovely ladies in Ahmedabad since 1991. We provide Laser Treatment, Hair Removal Services, Facial, Bridal and many more" />
+                <meta name='title' content='Vaidehi Beauty Care & Spa' />
+                <meta name='description' content='Vaidehi Beauty Care & Spa has been offering beauty services to the lovely ladies in Ahmedabad since 1991. We provide Laser Treatment, Hair Removal Services, Facial, Bridal and many more' />
 
-                {/* <!-- Open Graph / Facebook --> */}
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://www.vaidehibeautycare.com/" />
-                <meta property="og:title" content="Vaidehi Beauty Care & Spa" />
-                <meta property="og:description" content="Vaidehi Beauty Care & Spa has been offering beauty services to the lovely ladies in Ahmedabad since 1991. We provide Laser Treatment, Hair Removal Services, Facial, Bridal and many more" />
-                <meta property="og:image" content="" />
+                <meta property='og:type' content='website' />
+                <meta property='og:url' content='https://www.vaidehibeautycare.com/' />
+                <meta property='og:title' content='Vaidehi Beauty Care & Spa' />
+                <meta property='og:description' content='Vaidehi Beauty Care & Spa has been offering beauty services to the lovely ladies in Ahmedabad since 1991. We provide Laser Treatment, Hair Removal Services, Facial, Bridal and many more' />
+                <meta property='og:image' content='' />
 
-                {/* <!-- Twitter --> */}
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://www.vaidehibeautycare.com/" />
-                <meta property="twitter:title" content="Vaidehi Beauty Care & Spa" />
-                <meta property="twitter:description" content="Vaidehi Beauty Care & Spa has been offering beauty services to the lovely ladies in Ahmedabad since 1991. We provide Laser Treatment, Hair Removal Services, Facial, Bridal and many more" />
-                <meta property="twitter:image" content="" />
+                <meta property='twitter:card' content='summary_large_image' />
+                <meta property='twitter:url' content='https://www.vaidehibeautycare.com/' />
+                <meta property='twitter:title' content='Vaidehi Beauty Care & Spa' />
+                <meta property='twitter:description' content='Vaidehi Beauty Care & Spa has been offering beauty services to the lovely ladies in Ahmedabad since 1991. We provide Laser Treatment, Hair Removal Services, Facial, Bridal and many more' />
+                <meta property='twitter:image' content='' />
+                <Script
+                    id='google-tag-manager'
+                    strategy='beforeInteractive'
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MK2P3L2');`,
+                    }}
+                />
             </Head>
             <body className='antialiasing'>
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MK2P3L2" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+                    }}
+                />
                 <Main />
                 <NextScript />
             </body>
