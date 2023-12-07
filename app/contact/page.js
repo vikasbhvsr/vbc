27f4709fc5e2e3f16a7b2f2363dd5fec3c7108de
Vikas Bhavsar <vikasbhvsr@gmail.com>
2023-12-06 22:43:36 -0800

    Update contact phone number and layout

--- app/contact/page.js
+++ app/contact/page.js
@@ -37,9 +37,28 @@ export default function Contact() {
                     S8/S9, Viharika Complex<br />Near Ankur Cross Rd, Naranpura<br />Ahmedabad, Gujarat 380013
                   </address>
                 </Link>
-                <div className='flex divide-x divide-pink-800 space-x-2'>
-                  <Link className='text-pink-800 hover:text-pink-700 font-medium mt-2 block' href='https://goo.gl/maps/yteuBkuL5xWJ1zaDA' target='_blank'>Get Directions</Link>
-                  <Link className='pl-2 text-pink-800 hover:text-pink-700 font-medium mt-2 block' href='tel:+919825411321'>Call: +91 98254 11321</Link>
+                <div className='flex flex-col items-start space-y-2 mt-4'>
+                  <div className='flex items-center gap-2 text-pink-800 hover:text-pink-700'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
+                      <path fillRule="evenodd" d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 009.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 002.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3zm3-6a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v3a.75.75 0 01-.75.75h-3a.75.75 0 01-.75-.75v-3zm8.25-.75a.75.75 0 00-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 00.75-.75v-5.25a.75.75 0 00-.75-.75h-3z" clipRule="evenodd" />
+                    </svg>
+                    <Link className='font-medium block' href='https://goo.gl/maps/yteuBkuL5xWJ1zaDA' target='_blank'>Get Directions</Link>
+                  </div>
+                  <div className='flex items-center gap-2 text-pink-800 hover:text-pink-700'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+                      <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clipRule="evenodd" />
+                    </svg>
+                    <Link className='font-medium block' href='tel:+919825411321'>Appointment: +91 98254 11321</Link>
+                  </div>
+                  <div className='flex items-center gap-2 text-pink-800 hover:text-pink-700'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path fillRule="evenodd" d="M15 3.75a.75.75 0 01.75-.75h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0V5.56l-4.72 4.72a.75.75 0 11-1.06-1.06l4.72-4.72h-2.69a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+                      <path fillRule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clipRule="evenodd" />
+                    </svg>
+                    <Link className='font-medium block' href='tel:+919726011321'>Call: +91 97260 11321</Link>
+                  </div>
                 </div>
                 <iframe className='w-full h-64 max-w-full mt-6' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.0538987230966!2d72.55265027590151!3d23.058485479148594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e849d2fc020a5%3A0x9be8fc8e20faf638!2sVaidehi%20Beauty%20Care!5e0!3m2!1sen!2sus!4v1701490258492!5m2!1sen!2sus' allowFullScreen='' loading='lazy' referrerPolicy='no-referrer-when-downgrade'></iframe>
               </div>
@@ -58,9 +77,28 @@ export default function Contact() {
                     A/4, FF17, Arya Villa<br />Anand Party Plot Rd, New Ranip<br />Ahmedabad, Gujarat 382470
                   </address>
                 </Link>
-                <div className='flex divide-x divide-pink-800 space-x-2'>
-                  <Link className='text-pink-800 hover:text-pink-700 font-medium mt-2 block' href='https://goo.gl/maps/uMN82PpdWJ4ZQnVT8' target='_blank'>Get Directions</Link>
-                  <Link className='pl-2 text-pink-800 hover:text-pink-700 font-medium mt-2 block' href='tel:+919825411321'>Call: +91 98254 11321</Link>
+                <div className='flex flex-col items-start space-y-2 mt-4'>
+                  <div className='flex items-center gap-2 text-pink-800 hover:text-pink-700'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
+                      <path fillRule="evenodd" d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 009.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 002.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3zm3-6a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v3a.75.75 0 01-.75.75h-3a.75.75 0 01-.75-.75v-3zm8.25-.75a.75.75 0 00-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 00.75-.75v-5.25a.75.75 0 00-.75-.75h-3z" clipRule="evenodd" />
+                    </svg>
+                    <Link className='font-medium block' href='https://goo.gl/maps/uMN82PpdWJ4ZQnVT8' target='_blank'>Get Directions</Link>
+                  </div>
+                  <div className='flex items-center gap-2 text-pink-800 hover:text-pink-700'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+                      <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clipRule="evenodd" />
+                    </svg>
+                    <Link className='font-medium block' href='tel:+919825411321'>Appointment: +91 98254 11321</Link>
+                  </div>
+                  <div className='flex items-center gap-2 text-pink-800 hover:text-pink-700'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path fillRule="evenodd" d="M15 3.75a.75.75 0 01.75-.75h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0V5.56l-4.72 4.72a.75.75 0 11-1.06-1.06l4.72-4.72h-2.69a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+                      <path fillRule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clipRule="evenodd" />
+                    </svg>
+                    <Link className='font-medium block' href='tel:+919081011321'>Call: +91 90810 11321</Link>
+                  </div>
                 </div>
                 <iframe className='w-full max-w-full h-64 mt-6' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.2337452506044!2d72.55700277590216!3d23.08853767912753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e830c481d3603%3A0xea17de0365700683!2sVaidehi%20Beauty%20Care%20-%20Bridal%20Studio%20%26%20Academy%20-%20New%20Ranip!5e0!3m2!1sen!2sus!4v1701490412525!5m2!1sen!2sus' allowFullScreen='' loading='lazy' width='100%' height='100%' referrerPolicy='no-referrer-when-downgrade'></iframe>
               </div>
